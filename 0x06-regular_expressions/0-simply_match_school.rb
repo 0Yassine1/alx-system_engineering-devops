70e7cb946bd2bd0dd104b77cd56cabd318fb5b5c
yassine <yelkadiri39@gmail.com>
2024-11-07 14:13:41 +0100

    Fix output for School matching task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-
+# Accept input from the command line argument
 input = ARGV[0]
 
-
+# Output any matches for the word "School"
 if input
   puts input.scan(/School/).join
 end
